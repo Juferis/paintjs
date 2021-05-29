c3f4223301800f72468dcfd2e12a4851ff9b0eff
Juferis <66511872+Juferis@users.noreply.github.com>
2021-05-29 15:30:38 +0900

    2D Context and create painting!

--- Readme.md
+++ Readme.md
@@ -2,6 +2,13 @@
 
 > JS로 만든 그림판
 
+## 1.0 사용 기술
+
+### 1.1 canvas[MDN](https://developer.mozilla.org/ko/docs/Web/API/Canvas_API)
+
+> canvas는 html5가 가지고 있는 요소이고 canvas안에 있는 pixel들을 다룬다.  
+> canvas를 이용해 이벤트를 받아와 그림을 그린다.
+
 ## 만들어야 할 기능
 
 - 색상 변경 [ ]
--- app.js
+++ app.js
@@ -1,25 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
+const lineWidth = document.getElementById("jsRange");
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
 
 let painting = false;
+const ctx = canvas.getContext("2d");
+
+// 시작 색상을 검정으로 설정
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = lineWidth.value;
 
 const stopPainting = () => (painting = false);
+const startPainting = () => (painting = true);
 
 const handleMouseMove = (event) => {
   const offsetX = event.offsetX;
   const offsetY = event.offsetY;
-};
-
-const handleMouseDown = (event) => {
-  painting = true;
-};
-
-const handleMouseUp = (event) => {
-  stopPainting();
+  if (!painting) {
+    // 1. canvas를 클릭하지 않고 마우스를 올린 상태에서 움직이고 있는 동안 그릴 시작점을 정한다.
+    ctx.beginPath();
+    ctx.moveTo(offsetX, offsetY);
+  } else {
+    // 2. 마우스를 클릭 후 painting이 true가 되면 마우스 위치(beginPath)값을 기준으로 그림을 그리기 시작한다.
+    // 3. lineTo함수로 path의 위치가 계속 변경되며 이전 path의 위치와 지금 path위치를 선으로 잇는다.
+    // 그래서 그림을 그리는 것처럼 나온다.
+    ctx.lineTo(offsetX, offsetY);
+    ctx.stroke();
+  }
 };
 
 if (canvas) {
   canvas.addEventListener("mousemove", handleMouseMove);
-  canvas.addEventListener("mousedown", handleMouseDown);
-  canvas.addEventListener("mouseup", handleMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
